5c66360fa4598a2118c4ed855772b4e82664d434
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-06-22 13:10:57 +0200

    update

--- TestBotDiscord/src/main/java/fr/tyrolium/maxime/bot/main.java
+++ TestBotDiscord/src/main/java/fr/tyrolium/maxime/bot/main.java
@@ -5,18 +5,55 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 
 import javax.security.auth.login.LoginException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
 
 public class main {
-    public static void main(String[] argv) throws LoginException {
+    public static void main(String[] argv) throws LoginException, IOException {
         JDABuilder builder = new JDABuilder(AccountType.BOT);
-        builder.setToken(stock.token);
+        builder.setToken(stock.tokenServ);
         builder.addEventListeners(new test());
-        builder.setActivity(Activity.watching("JE SUIS EN DEV WHOUUUAAA"));
+        builder.setActivity(Activity.playing("TyroServ.fr | ONLINE"));
         builder.build();
+        System.out.println("Le bot c'est bien lanceer");
 
-        System.out.println("Le bot c'est bien lancé");
+
+        String onlineServ = get("http://tyrolium.fr/Contenu/Php/api/status.php");
+        System.out.println(onlineServ);
+        while (onlineServ != ""){
+            onlineServ = get("http://tyrolium.fr/Contenu/Php/api/status.php");
+            if (onlineServ == ""){
+                System.out.println("pas de co");
+                builder.setActivity(Activity.watching("TyroServ.fr | OFFLINE"));
+                builder.build();
+            }else {
+                System.out.println("bien co");
+            }
+        }
+        while (onlineServ == ""){
+            onlineServ = get("http://tyrolium.fr/Contenu/Php/api/status.php");
+            if (onlineServ == ""){
+                System.out.println("pas de co");
+            }else {
+                System.out.println("bien co");
+            }
+        }
     }
+    public static String get(String url) throws IOException{
 
-    public static String Prefix = "test!";
-   // public static Integer PapaId = 363366883652796416;
+        String source ="";
+        URL oracle = new URL(url);
+        URLConnection yc = oracle.openConnection();
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(
+                        yc.getInputStream()));
+        String inputLine;
+
+        while ((inputLine = in.readLine()) != null)
+            source +=inputLine;
+        in.close();
+        return source;
+    }
 }
--- TestBotDiscord/src/main/java/fr/tyrolium/maxime/bot/test.java
+++ TestBotDiscord/src/main/java/fr/tyrolium/maxime/bot/test.java
@@ -7,43 +7,106 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.annotation.Nonnull;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class test extends ListenerAdapter {
     @Override
     public void onMessageReceived(@Nonnull MessageReceivedEvent event) {
+
+        String Pref = "ts!";
         User userTab = event.getAuthor();
         MessageChannel channelTab = event.getChannel();
         Guild guildTab = event.getGuild();
         System.out.println(guildTab.getName() + " / " + channelTab.getName() + " / " + userTab.getAsTag() + " ----> " + event.getMessage().getContentRaw());
 
-
-        if (event.getMessage().getContentRaw().toLowerCase().contains("on peut sortir ?")) {
-            event.getChannel().sendMessage("Non. #RestezChezVous").queue();
+        if (event.getMessage().getContentRaw().toLowerCase().contains(Pref + "help")) {
+            event.getChannel().sendMessage("``` [ts!] help = cette commande```").queue();
+            event.getChannel().sendMessage("``` [ts!] status = status du serveur minecraft```").queue();
+            event.getChannel().sendMessage("``` [ts!] ip = ip du serv meme si y'en a pas ahah```").queue();
+            event.getChannel().sendMessage("``` [ts!] site = le site web du serv minecraft```").queue();
             System.out.println(guildTab.getName() + " / " + channelTab.getName() + " <Requette Effectuez>");
         }
 
-        if (event.getMessage().getContentRaw().toLowerCase().contains(main.Prefix + "help")) {
-            event.getChannel().sendMessage("hum ya pas grand chose dans le bot pour l'instant").queue();
+        if (event.getMessage().getContentRaw().toLowerCase().contains(Pref + "status")) {
+            String result = null;
+            try {
+                result = get("http://tyrolium.fr/Contenu/Php/api/status.php");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (result == ""){
+                    result = "SERVEUR OFFLINE";
+                }else {
+                    result = "SERVEUR ONLINE";
+                }
+            event.getChannel().sendMessage("__**STATUS**__").queue();
+            event.getChannel().sendMessage("``" + result + "``").queue();
+        }
+
+        if (event.getMessage().getContentRaw().toLowerCase().contains(Pref + "ip")) {
+            event.getChannel().sendMessage("__**IP**__").queue();
+            event.getChannel().sendMessage("``le serveur ne possede pas d'ip il faut installer un launcher sur``"+ "http://tyroserv.fr/").queue();
             System.out.println(guildTab.getName() + " / " + channelTab.getName() + " <Requette Effectuez>");
         }
 
-        if (event.getMessage().getContentRaw().toLowerCase().contains("je suis qui ?")) {
-            event.getChannel().sendMessage("vous etes" + userTab.getName()).queue();
+        if (event.getMessage().getContentRaw().toLowerCase().contains(Pref + "site")) {
+            event.getChannel().sendMessage("__**SITE WEB**__").queue();
+            event.getChannel().sendMessage("http://tyroserv.fr/").queue();
             System.out.println(guildTab.getName() + " / " + channelTab.getName() + " <Requette Effectuez>");
         }
 
-        if (event.getMessage().getContentRaw().toLowerCase().contains("ya combien de membre sur le serveur ?")) {
-            event.getChannel().sendMessage("btn").queue();
-            System.out.println(guildTab.getName() + " / " + channelTab.getName() + " <Requette Effectuez>");
+        if (event.getMessage().getContentRaw().toLowerCase().contains(Pref + "player")) {
+            String result = null;
+            String result2 = null;
+            try {
+                result = get("http://tyrolium.fr/Contenu/Php/api/status.php");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (result == ""){
+                event.getChannel().sendMessage("__**STATUS**__").queue();
+                event.getChannel().sendMessage("``" + result + "``").queue();
+            }else {
+                try {
+                    result2 = get("http://tyrolium.fr/Contenu/Php/api/player.php");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                event.getChannel().sendMessage("__**NOMBRE DE PLAYER CONNECTER**__").queue();
+                event.getChannel().sendMessage("``" + result2 + "``").queue();
+            }
         }
 
 
-        if (event.getMessage().getContentRaw().toLowerCase().contains(main.Prefix + "debug")) {
-            Number userId = userTab.getIdLong();
-          /*if (userTab == Papa){
-                event.getChannel().sendMessage("Coucou maxime").queue();
-            }*/
-            event.getChannel().sendMessage("__Debug__" + userTab + " " + userId).queue();
-        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+    public static String get(String url) throws IOException {
+
+        String source ="";
+        URL oracle = new URL(url);
+        URLConnection yc = oracle.openConnection();
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(
+                        yc.getInputStream()));
+        String inputLine;
+
+        while ((inputLine = in.readLine()) != null)
+            source +=inputLine;
+        in.close();
+        return source;
     }
 }
